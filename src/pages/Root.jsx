aba2802ff836d585f82cbb432c5273a18a67a9dc
Haris <harissetiawan522@gmail.com>
2024-10-08 22:51:14 +0700

    second commit

--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -72,16 +72,6 @@ const Root = () => {
                 </svg>
                 <span className="sr-only">Search icon</span>
               </div>
-              {/* <input
-                type="text"
-                id="search-navbar"
-                className="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                placeholder="Search..."
-                onChange={(e) => {
-                  setSearchWord(e.target.value);
-                }}
-                value={searchWord}
-              /> */}
               <Form
                 id="search-form"
                 role="search"
